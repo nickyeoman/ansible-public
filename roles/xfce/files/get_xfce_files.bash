e3f09252db9d07417ad004d0f5e7435be186fb66
nick <c@nickyeoman.com>
2020-03-30 17:07:11 -0700

    couldn't figure out how to ansible xfce settting so will have to use xfce-panel-switch

--- roles/xfce/files/get_xfce_files.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Copies your current xfce into this files directory
-
-cp -R ~/.config/Thunar/ ~/Git/ansible-public/roles/xfce/files/Thunar/
-cp -R ~/.config/xfce4/ ~/Git/ansible-public/roles/xfce/files/xfce4/
